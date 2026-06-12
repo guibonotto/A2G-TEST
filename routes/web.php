b9b7c08ab49e77704c5ab4ff115687035e8f2c80
Guilherme Bonotto <gama.guilherme@pucpr.edu.br>
2026-06-12 11:25:57 -0300

    Implementação de toda a Sprint 2 dentro das Views

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+
+    Route::get('/', function () {
+    return view('index');
+});
 });
 
 require __DIR__.'/settings.php';
